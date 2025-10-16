e30167e348c56e3902885707cb91fe83e500a292
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-10-16 13:51:09 +0900

    Scan for functions

--- src/extension.js
+++ src/extension.js
@@ -1,28 +1,48 @@
 const vscode = require("vscode");
 
 function activate(context) {
-    console.log("BRC extension activated");
-
-    vscode.window.showInformationMessage('Hello World!');
-}
-
-/*export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
         vscode.languages.registerDocumentSymbolProvider(
             {scheme: "file", language: "brc"},
             new BrcDocumentSymbolProvider()
         )
     );
-    console.log("BRC extension active");
-}*/
+}
 
-/*export class BrcDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+// implements vscode.DocumentSymbolProvider
+class BrcDocumentSymbolProvider {
+    provideDocumentSymbols(document, token) {
+        let symbols = getSymbols(document);
+        return symbols;
+    }
+}
 
-}*/
+function getSymbols(document) {
+    let symbols = [];
 
-function deactivate() { }
+    for (let i=0; i<document.lineCount; i++) {
+        let line = document.lineAt(i);
+        let match = line.text.match("(\\S+)\\s+fun");
+
+        if (match) {
+            let range = new vscode.Range(
+                new vscode.Position(i, match.index),
+                new vscode.Position(i, match.index + match[1].length)
+            );
+            let symbol = new vscode.DocumentSymbol(
+                match[1],
+                null,
+                vscode.SymbolKind.Function,
+                range,
+                range
+            );
+            symbols.push(symbol);
+        }
+    }
+
+    return symbols;
+}
 
 module.exports = {
-    activate,
-    deactivate
+    activate
 }
