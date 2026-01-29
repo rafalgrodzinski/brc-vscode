40c6e8e126702e0524c0d282f51f9e5fa6750054
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-01-29 16:00:59 +0900

    Simple outline

--- src/extension.js
+++ src/extension.js
@@ -1,8 +1,9 @@
 const vscode = require("vscode");
 
 class SourceToken {
-    constructor(startIndex, length, lexme) {
-        this.startIndex = startIndex;
+    constructor(line, column, length, lexme) {
+        this.line = line;
+        this.column = column;
         this.length = length;
         this.lexme = lexme;
     }
@@ -11,8 +12,8 @@ class SourceToken {
 // implements vscode.DocumentSymbolProvider
 class BrcDocumentSymbolProvider {
     provideDocumentSymbols(document, token) {
-        let sourceTokens = getSourceTokens(document)
-        let symbols = getSymbols(document);
+        let sourceTokens = getSourceTokens(document);
+        let symbols = getSymbols(sourceTokens);
         return symbols;
     }
 }
@@ -29,26 +30,28 @@ function activate(context) {
 function getSourceTokens(document) {
     let sourceTokens = [];
 
-    let source = document.getText();
-    let currentIndex = 0;
+    for (let line=0; line<document.lineCount; line++) {
+        let sourceLine = document.lineAt(line).text;
+        let column = 0;
 
-    while (currentIndex < source.length) {
-        // skip white spaces
-        while (source[currentIndex] == ' ' || source[currentIndex] == '\t')
-            currentIndex++;
+        while (column < sourceLine.length) {
+            // skip white spaces
+            while (sourceLine[column] == ' ' || sourceLine[column] == '\t')
+                column++;
 
-        let tokenStart = currentIndex;
+            let tokenColumn = column;
 
-        let count = tryMatchingOneOf(source, currentIndex, ["/*", "*/", "//", "\n"]);
-        if (count != 0) {
-            currentIndex += count;
-        } else {
-            while (tryMatchingOneOf(source, currentIndex, [" ", "\t", "/*", "*/", "\n"]) == 0 && currentIndex < source.length)
-                currentIndex++; 
+            let count = tryMatchingOneOf(sourceLine, column, ["/*", "*/", "//", "\n"]);
+            if (count != 0) {
+                column += count;
+            } else {
+                while (tryMatchingOneOf(sourceLine, column, [" ", "\t", "/*", "*/", "\n"]) == 0 && column < sourceLine.length)
+                    column++; 
+            }
+
+            let sourceToken = new SourceToken(line, tokenColumn, column - tokenColumn, sourceLine.substring(tokenColumn, column));
+            sourceTokens.push(sourceToken);
         }
-
-        let sourceToken = new SourceToken(tokenStart, currentIndex - tokenStart, source.substring(tokenStart, currentIndex));
-        sourceTokens.push(sourceToken);
     }
 
     return sourceTokens;
@@ -76,7 +79,114 @@ function tryMatching(source, currentIndex, lexme) {
     return lexme.length;
 }
 
-function getSymbols(document) {
+function getSymbols(sourceTokens) {
+    let symbols = [];
+
+    let commentDepth = 0;
+    let isLineComment = false;
+    let parentSymbol = null;
+
+    for (let i=0; i<sourceTokens.length; i++) {
+        let symbolFound = false;
+
+        let symbolName;
+        let symbolDetail;
+        let symbolKind;
+
+        let symbolLine;
+        let symbolColumn;
+        let symbolLength;
+
+        if (sourceTokens[i].lexme == "/*") {
+            commentDepth++;
+        } else if (sourceTokens[i].lexme == "*/") {
+            commentDepth--;
+        } else if (sourceTokens[i].lexme == "//") {
+            isLineComment = true;
+        } else if (sourceTokens[i].lexme == "\n") {
+            isLineComment = false;
+        } else if (!isLineComment && commentDepth == 0) {
+            // fun
+            if ((sourceTokens[i].lexme == "fun" || sourceTokens[i].lexme == "fun:" ) && i > 0) {
+                symbolFound = true;
+
+                symbolName = sourceTokens[i-1].lexme;
+                symbolDetail = "fun";
+                symbolKind = vscode.SymbolKind.Function;
+
+                if (i > 1 && sourceTokens[i-2].lexme == "@export") {
+                    symbolDetail += ", @export";
+                    symbolColumn = sourceTokens[i-2].column;
+                } else {
+                    symbolColumn = sourceTokens[i-1].column;
+                }
+                
+                symbolLine = sourceTokens[i].line;
+                symbolLength = sourceTokens[i].column + sourceTokens[i].length - symbolColumn;
+            // raw
+            } else if (sourceTokens[i].lexme == "raw" && i > 0) {
+
+            // blob
+            } else if (sourceTokens[i].lexme == "blob" && i > 0) {
+                symbolFound = true;
+
+                symbolName = sourceTokens[i-1].lexme;
+                symbolDetail = "blob";
+                symbolKind = vscode.SymbolKind.Struct;
+
+                symbolLine = sourceTokens[i-1].line;
+                symbolColumn = sourceTokens[i-1].column;
+                symbolLength = sourceTokens[i].column + sourceTokens[i].length - symbolColumn;
+            // module
+            } else if (sourceTokens[i].lexme == "@module" && i < sourceTokens.length - 1) {
+                symbolFound = true;
+
+                symbolName = sourceTokens[i+1].lexme;
+                symbolDetail = "@module";
+                symbolKind = vscode.SymbolKind.File;
+
+                symbolLine = sourceTokens[i].line;
+                symbolColumn = sourceTokens[i].column;
+                symbolLength = sourceTokens[i+1].column + sourceTokens[i+1].length - symbolColumn;
+            // import
+            } else if (sourceTokens[i].lexme == "@import" && i < sourceTokens.length - 1) {
+                symbolFound = true;
+
+                symbolName = sourceTokens[i+1].lexme;
+                symbolDetail = "@import";
+                symbolKind = null;
+
+                symbolLine = sourceTokens[i].line;
+                symbolColumn = sourceTokens[i].column;
+                symbolLength = sourceTokens[i+1].column + sourceTokens[i+1].length - symbolColumn;
+            }
+
+            if (symbolFound) {
+                let range = new vscode.Range(
+                    new vscode.Position(symbolLine, symbolColumn),
+                    new vscode.Position(symbolLine, symbolColumn + symbolLength)
+                );
+                let symbol = new vscode.DocumentSymbol(
+                    symbolName,
+                    symbolDetail,
+                    symbolKind,
+                    range,
+                    range
+                );
+
+                if (parentSymbol) {
+                    parentSymbol.children.push(symbol);
+                } else {
+                    symbols.push(symbol);
+                }
+            }
+        }
+    }
+
+    return symbols;
+}
+
+/*function getSymbols(document) {
     let symbols = [];
     let alreadyFoundFun = false;
 
@@ -149,7 +259,7 @@ function getSymbols(document) {
     }
 
     return symbols;
-}
+}*/
 
 module.exports = {
     activate
